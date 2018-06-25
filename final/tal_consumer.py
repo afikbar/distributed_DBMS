c15619d546f119de866b0a26a4275306b9c54861
Afik Bar <afikbar@gmail.com>
2018-06-25 12:13:26 +0300

    [final] bugfix with strptime :bug:

--- final/tal_consumer.py
+++ final/tal_consumer.py
@@ -17,24 +17,24 @@ SCHEMA_DEF = ['id', 'stop_date', 'location_raw', 'driver_gender',
                 'driver_race', 'violation', 'search_conducted', 'is_arrested']   
 
 def msg_cleaner(msg):
-    result = re.sub("[()']",'',msg.decode('utf8')).split(",")
+    result = re.sub("[()']",'',msg.value.decode('utf8')).split(",")
     return [word.strip() for word in result]
 
 def str_to_bool(str):
     return str.lower() == "true"
 
 def data_filter(msg_dict):#returns true if data is ok
-    return (msg_dict['location_raw'].title() 
-                in ['San Diego', 'Redding', 'Buttonwillow', 'Modesto']) and
-            (msg_dict['search_conducted'].lower() in ['true','false']) and  
-            (msg_dict['is_arrested'].lower() in ['true','false']) and 
-            (msg_dict['driver_gender'].lower() in ['m','f']) and 
-            (re.match('(\d{4})[/.-](\d{2})[/.-](\d{2})$', msg_dict['stop_date']))
+    
+    return msg_dict['location_raw'].title() in ['San Diego', 'Redding', 'Buttonwillow', 'Modesto'] and\
+            msg_dict['search_conducted'].lower() in ['true','false'] and\
+            msg_dict['is_arrested'].lower() in ['true','false'] and\
+            msg_dict['driver_gender'].lower() in ['m','f'] and\
+            re.match('(\d{4})[/.-](\d{2})[/.-](\d{2})$', msg_dict['stop_date'])
                         
 def data_convert(msg_dict):
     msg_dict['search_conducted'] = str_to_bool(msg_dict['search_conducted'])
     msg_dict['is_arrested'] = str_to_bool(msg_dict['is_arrested'])
-    msg_dict['stop_date'] = datetime.strptime(msg_dict['stop_date'], "%Y-%M-%D")
+    msg_dict['stop_date'] = datetime.strptime(msg_dict['stop_date'], "%Y-%m-%d")
     msg_dict['driver_gender'] = msg_dict['driver_gender'].upper()
     msg_dict['location_raw'] = msg_dict['location_raw'].title()
     
@@ -42,18 +42,19 @@ def tal_consumer(ip, port, topic):
     consumer = KafkaConsumer(topic, 
                             bootstrap_servers='{}:{}'.format(ip,port), 
                             auto_offset_reset='earliest')
-    first = next(consumer).value
+    first = next(consumer)
    
     schema = msg_cleaner(first)
     
     es = Elasticsearch() 
     i=1
     for msg in consumer:
-  
+          
         val_ls = msg_cleaner(msg)
         #keeps only wanted data
-        msg_dict = dict((k,v) for k,v in zip(schema,val_ls) if k in SCHEMA_DEF)
-        if data_filter(msg) == FALSE: 
+        msg_dict = dict([(k,v) for k,v in zip(schema,val_ls) if k in SCHEMA_DEF])
+#         temp = msg_dict['id']
+        if not data_filter(msg_dict): 
             continue
         
         data_convert(msg_dict)
@@ -63,8 +64,8 @@ def tal_consumer(ip, port, topic):
         loc_json = json.dumps({"location_raw":msg_dict["location_raw"]})
         es.index(index='buttonwillow', doc_type='json', id=i, body=loc_json)
         # black panther - redding:
-        if msg_dict['search_conducted'] 
-            and msg_dict['stop_date'] >= datetime(2016,1,30):
+        if msg_dict['search_conducted'] and\
+            msg_dict['stop_date'] >= datetime(2016,1,30):
             bp_json = json.dumps(msg_dict)
             es.index(index='reddings', doc_type='json', id=i, body=bp_json)
             
